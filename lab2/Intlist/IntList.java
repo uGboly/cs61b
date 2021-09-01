1248532f26c2d3ef3cf45e4af5878a33a34ecea5
Loser Zhang <ZhangLin@A-MacBook-Air.local>
2021-09-01 15:58:29 +0800

    lab2 complete

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,13 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList ptr=A;
+
+        while (ptr.rest!=null){
+            ptr=ptr.rest;
+        }
+        ptr.rest=B;
+        return A;
     }
 
     /**
@@ -91,7 +97,11 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A==null){
+            return B;
+        }
+
+        return new IntList(A.first, catenate(A.rest,B));
     }
 
 
