eefcad121c73b64bed9f87c6974984c1b1be3666
Loser Zhang <ZhangLin@A-MacBook-Air.local>
2021-09-05 21:08:46 +0800

    revise get

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -78,6 +78,9 @@ public class ArrayDeque <T> {
     }
 
     public  T get(int index) {
+        if (index >= size) {
+            return null;
+        }
         return items[addOne(nextFirst + index)];
     }
 
