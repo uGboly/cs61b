353ee67e61424d13f9dc153324049f22025cfd6b
Loser Zhang <ZhangLin@A-MacBook-Air.local>
2021-09-05 21:05:38 +0800

    remake ad

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,79 +1,122 @@
-public class ArrayDeque <T>{
+public class ArrayDeque <T> {
     private T[] items;
     private int size;
-    private int first;
+    private int nextFirst;
+    private int nextLast;
 
-    public ArrayDeque(){
-        items = (T[]) new Object[100];
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
         size = 0;
+        nextFirst = 0;
+        nextLast = 1;
     }
 
-    public int size(){
+    public int size() {
         return size;
+
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public ArrayDeque(ArrayDeque other){
+    public int addOne(int a) {
+        return (a + 1) % items.length;
+    }
+
+    private int subOne(int a) {
+        return (a - 1 + items.length) % items.length;
+    }
+
+    private void resize(int sz) {
+        T[] newItems = (T[]) new Object[sz];
+        int start = addOne(nextFirst);
+
+        for(int i = 0; i < size; i++) {
+            newItems[i] = items[addOne(start+i)];
+        }
+
+        items = newItems;
+        nextFirst = sz - 1;
+        nextLast = size;
+    }
+
+
+
+    private ArrayDeque(ArrayDeque<T> other) {
         size = other.size;
         items = (T[]) new Object[other.items.length];
+        int start = addOne(other.nextFirst);
 
-        System.arraycopy(other.items,0,items,0,size);
+        for(int i = 0; i < size; i++ ) {
+            items[i] = other.items[addOne(start+i)];
+        }
+
+        nextFirst = items.length - 1;
+        nextLast = size;
     }
+
+
 
     public void addLast(T x) {
         if(size == items.length){
-            T[] temp = (T[]) new Object[items.length*2];
-            System.arraycopy(items,0,temp,0,size);
-
-            items = temp;
+            resize(size*2);
         }
-        items[size] = x;
+        items[nextLast] = x;
+        nextLast = addOne(nextLast);
         size++;
     }
 
     public void addFirst(T x) {
         if(size == items.length){
-            T[] temp = (T[]) new Object[items.length*2];
-            System.arraycopy(items,0,temp,1,size);
-
-            items = temp;
+            resize(size*2);
         }
+
+        items[nextFirst] = x;
+        nextFirst = subOne(nextFirst);
         size++;
-        items[0] = x;
     }
 
-    public  T get(int index){
-        return items[index];
+    public  T get(int index) {
+        return items[addOne(nextFirst + index)];
     }
 
-    public T removeFirst(){
-        T firstitem = get(0);
+    public void printDeque() {
+        int i = addOne(nextFirst);
+        for (int j = 0; j < size; j++) {
+            System.out.print(items[i] + " ");
+            i = addOne(i);
+        }
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        T rm = items[addOne(nextFirst)];
+        nextFirst = subOne(nextFirst);
         size--;
 
-        for (int i = 0; i < size-1; i++){
-            items[i]=items[i+1];
-        }
 
-        if (items.length > 200 && size / items.length<0.25){
-            T[] temp = (T[]) new Object[items.length/2];
-            System.arraycopy(items,0,temp,0,size);
-            items = temp;
+        if (items.length >= 16 && size < (items.length/4)) {
+            resize(size/2);
         }
-
-        return firstitem;
+        return rm;
     }
 
-    public T removeLast(){
-        size--;
-        if (items.length > 200 && size / items.length<0.25){
-            T[] temp = (T[]) new Object[items.length/2];
-            System.arraycopy(items,0,temp,0,size);
-            items = temp;
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
         }
 
-        return items[size];
+        T rm = items[subOne(nextLast)];
+        nextLast = addOne(nextLast);
+        size--;
+
+        if (items.length >= 16 && size < (items.length/4)) {
+            resize(size/2);
+        }
+        return rm;
     }
 }
