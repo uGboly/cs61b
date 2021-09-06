0cf7331caece6c5e7be9d6a5bc7bae9822a8443a
Loser Zhang <ZhangLin@A-MacBook-Air.local>
2021-09-06 11:11:16 +0800

    api

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,7 +1,7 @@
 public class OffByN implements CharacterComparator{
-    int n;
+    private int n;
 
-    OffByN(int N){
+    public OffByN(int N){
         n=N;
     }
 
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -3,8 +3,8 @@ public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y){
         if(Math.abs(x - y) == 1) {
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 }
