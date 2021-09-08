04c332613fe8dee57a69ef221bace992fb0252bf
Loser Zhang <ZhangLin@A-MacBook-Air.local>
2021-09-08 17:03:38 +0800

    hw1 completed

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -1,11 +1,11 @@
 package es.datastructur.synthesizer;
 import java.util.Iterator;
 
-//TODO: Make sure to that this class and all of its methods are public
-//TODO: Make sure to add the override tag for all overridden methods
-//TODO: Make sure to make this class implement BoundedQueue<T>
+//Make sure to that this class and all of its methods are public
+//Make sure to add the override tag for all overridden methods
+//Make sure to make this class implement BoundedQueue<T>
 
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T>  implements BoundedQueue<T>{
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
@@ -15,12 +15,35 @@ public class ArrayRingBuffer<T>  {
     /* Array for storing the buffer data. */
     private T[] rb;
 
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+
+    @Override
+    public int capacity(){
+        return rb.length;
+    }
+
+
+    private int subOne(int x) {
+        return (x - 1 + capacity()) % capacity();
+    }
+
+    private int addOne(int x) {
+        return (x + 1) % capacity();
+    }
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        // TODO: Create new array with capacity elements.
+        //  Create new array with capacity elements.
         //       first, last, and fillCount should all be set to 0.
+        first = 0;
+        last = 0;
+        fillCount = 0;
+
+        rb = (T[]) new Object[capacity] ;
     }
 
     /**
@@ -28,9 +51,15 @@ public class ArrayRingBuffer<T>  {
      * throw new RuntimeException("Ring buffer overflow").
      */
     public void enqueue(T x) {
-        // TODO: Enqueue the item. Don't forget to increase fillCount and update
+        //  Enqueue the item. Don't forget to increase fillCount and update
         //       last.
-        return;
+        if (isFull()) {
+            throw new RuntimeException("Ring Buffer overflow");
+        }
+
+        fillCount--;
+        rb[last] = x;
+        last = addOne(last);
     }
 
     /**
@@ -38,9 +67,17 @@ public class ArrayRingBuffer<T>  {
      * throw new RuntimeException("Ring buffer underflow").
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and
+        //  Dequeue the first item. Don't forget to decrease fillCount and
         //       update first.
-        return null;
+
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer underflow");
+        }
+
+        fillCount--;
+        T temp = rb[first];
+        first = addOne(first);
+        return temp;
     }
 
     /**
@@ -48,12 +85,56 @@ public class ArrayRingBuffer<T>  {
      * throw new RuntimeException("Ring buffer underflow").
      */
     public T peek() {
-        // TODO: Return the first item. None of your instance variables should
+        // Return the first item. None of your instance variables should
         //       change.
-        return null;
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer underflow");
+        }
+
+        return rb[first];
     }
 
-    // TODO: When you get to part 4, implement the needed code to support
+    // When you get to part 4, implement the needed code to support
     //       iteration and equals.
+    private class ArbIterator implements Iterator<T>{
+        int pos;
+
+        public ArbIterator(){
+            pos = 0;
+        }
+
+        public boolean hasNext(){
+            return pos < fillCount();
+        }
+
+        public T next(){
+            T temp = rb[addOne(pos+first)];
+            pos++;
+            return temp;
+        }
+    }
+
+    public Iterator<T> iterator(){
+        return new ArbIterator();
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if (this == other) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+
+        ArrayRingBuffer<T> o = (ArrayRingBuffer<T>) other;
+        if(!o.rb.equals(this.rb)){
+            return false;
+        }
+        return true;
+    }
+
 }
-    // TODO: Remove all comments that say TODO when you're done.
--- hw1/es/datastructur/synthesizer/GuitarString.java
+++ hw1/es/datastructur/synthesizer/GuitarString.java
@@ -12,36 +12,48 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
+        // Create a buffer with capacity = SR / frequency. You'll need to
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your buffer should be initially filled with zeros.
+        buffer = new ArrayRingBuffer<Double>((int)Math.round(SR / frequency));
+        for (int i = 0; i < buffer.capacity(); i++){
+            buffer.enqueue(0.0);
+        }
     }
 
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
+        // Dequeue everything in buffer, and replace with random numbers
         //       between -0.5 and 0.5. You can get such a number by using:
         //       double r = Math.random() - 0.5;
         //
         //       Make sure that your random numbers are different from each
         //       other.
+        for (int i = 0; i < buffer.capacity(); i++){
+            buffer.dequeue();
+            buffer.enqueue(Math.random() - 0.5);
+        }
+
     }
 
     /* Advance the simulation one time step by performing one iteration of
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
+        // Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
+
+        double temp = buffer.dequeue();
+        buffer.enqueue(0.996 * (temp + buffer.peek()) / 2);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        // Return the correct thing.
+        return buffer.peek();
     }
 }
     // TODO: Remove all comments that say TODO when you're done.
