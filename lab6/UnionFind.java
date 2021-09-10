45e450e5bd1b413de742d99911057bb8c1e4344a
Loser Zhang <ZhangLin@A-MacBook-Air.local>
2021-09-10 09:48:57 +0800

    lab6 completed

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,34 +1,49 @@
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    int[] ar;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        ar = new int[n];
+        for (int i = 0;i < n;i++){
+            ar[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex < 0 || vertex > ar.length-1) {
+            throw new RuntimeException("Index is invalid");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+
+        if(ar[v1] < 0){
+            return ar[v1] * -1;
+        }
+        return sizeOf(ar[parent(v1)]);
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+
+        return ar[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
+        validate(v1);
+        validate(v2);
+
+        if(find(v1) == find(v2)){
+            return true;
+        }
         return false;
     }
 
@@ -38,14 +53,38 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        validate(v1);
+        validate(v2);
+
+        int r1 = find(v1);
+        int r2 = find(v2);
+
+        if(r1 != r2) {
+            if (sizeOf(r1) > sizeOf(r2)) {
+                ar[r1] += ar[r2];
+                ar[r2] = r1;
+            } else {
+                ar[r2] += ar[r1];
+                ar[r1] = r2;
+            }
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        int v = vertex;
+
+        while (parent(v) >= 0){
+            v = ar[v];
+        }
+
+        while (parent(vertex) >= 0){
+            ar[vertex] = v;
+            vertex = ar[vertex];
+        }
+
+        return v;
     }
 
 }
